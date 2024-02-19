3379469f6d3a3df0240ebe8ddeb2143768be9cc4
developersihyeon <139722653+developersihyeon@users.noreply.github.com>
2024-02-20 02:40:00 +0900

    remove :: Topbar 관련 폴더 & 파일 삭제 #5

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,11 +1,9 @@
 import "src/style/Main.style/Main.style.css";
 import MainImage from "src/assets/img/Main_Image.png";
-import Header from "src/components/Header/Header";
 
 const Main = () => {
   return (
     <div className="MainPage">
-      <Header />
       <div className="Main">
         <img className="MainImage" src={MainImage} alt="메인 이미지"></img>
       </div>
