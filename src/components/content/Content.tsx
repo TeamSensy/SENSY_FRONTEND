62aa0182c3fe3e3b16fcfd9cfaaa0b329d746f68
이윤선 <108972919+yoonsun123@users.noreply.github.com>
2024-02-25 02:12:57 +0900

    더미데이터 생성, state
    더미데이터를 생성 후 state를 만들긴 했지만 아직 연결시키지 못함

--- src/components/content/Content.tsx
+++ src/components/content/Content.tsx
@@ -1,7 +1,17 @@
+import React, { useState } from "react";
+import ReactMarkdown from 'react-markdown'
+import { dummy } from "./data"
+
 const Content = () => {
+  const markdown = `Just a link: www.nasa.gov.`;
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [previewImg, setPreviewImg] = useState("");
+  const [contentImg, setContentImg] = useState("");
     return (
       <div>
-        <div></div>
+        <ReactMarkdown children={markdown} />
+        
       </div>
     );
   };
