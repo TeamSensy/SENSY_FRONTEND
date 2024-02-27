913cc7eb85d92c9b31b4e0caa82e5ff8f6bd12aa
developersihyeon <139722653+developersihyeon@users.noreply.github.com>
2024-02-27 21:26:20 +0900

    design :: Popular Courses 디자인 수정 #5

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -11,101 +11,93 @@ const Main = () => {
       <div className="Main">
         <img className="MainImage" src={MainImage} alt="메인 이미지"></img>
         <div className="PopularCoursesWrap">
-          <div className="PopularCoursesLine">
-            <div className="PopularCourses">
-              <div className="PopularCoursesTop">
-                <p className="PopularCoursesTitle">Popular Courses</p>
-                <p className="PopularCoursesDescription">
-                  Check out our best sellers
-                </p>
-                <div className="PopularCoursesButtonWrap">
-                  <div className="PopularCoursesButton">
-                    <button className="AddToCartButton">Add to Cart</button>
-                    <button className="ViewAllButton">View All</button>
+          <div className="PopularCourses">
+            <div className="PopularCoursesTop">
+              <p className="PopularCoursesTitle">Popular Courses</p>
+              <p className="PopularCoursesDescription">
+                Check out our best sellers
+              </p>
+              <div className="PopularCoursesButtonWrap">
+                <div className="PopularCoursesButton">
+                  <button className="AddToCartButton">Add to Cart</button>
+                  <button className="ViewAllButton">View All</button>
+                </div>
+              </div>
+            </div>
+            <div className="PopularCoursesBottom">
+              <img
+                className="PopularCoursesLeftIcon"
+                src={LeftIcon}
+                alt="왼쪽 아이콘"></img>
+              {/* Card_Dummy */}
+              <div className="PopularCoursesCard">
+                <img
+                  className="PopularCoursesCardImage"
+                  src={CardDummyImage}
+                  alt="카드 이미지"></img>
+                <div className="PopularCoursesCardInfo">
+                  <div className="PopularCoursesCardTitleWrap">
+                    <span className="PopularCoursesCardTitle">Product 1</span>
+                  </div>
+                  <div className="PopularCoursesCardContentWrap">
+                    <p className="PopularCoursesCardContent">Product 1 data</p>
+                    <div className="PopularCoursesCardLikeWrap">
+                      <img
+                        className="PopularCoursesCardLikeIcon"
+                        src={LikeIcon}
+                        alt="좋아요 아이콘"></img>
+                      <p className="PopularCoursesCardLike">Like</p>
+                    </div>
                   </div>
                 </div>
               </div>
-              <div className="PopularCoursesBottom">
+              <div className="PopularCoursesCard">
                 <img
-                  className="PopularCoursesLeftIcon"
-                  src={LeftIcon}
-                  alt="왼쪽 아이콘"></img>
-                {/* Card_Dummy */}
-                <div className="PopularCoursesCard">
-                  <img
-                    className="PopularCoursesCardImage"
-                    src={CardDummyImage}
-                    alt="카드 이미지"></img>
-                  <div className="PopularCoursesCardInfo">
-                    <div className="PopularCoursesCardTitleWrap">
-                      <span className="PopularCoursesCardTitle">Product 1</span>
-                    </div>
-                    <div className="PopularCoursesCardContentWrap">
-                      <p className="PopularCoursesCardContent">
-                        Product 1 data
-                      </p>
-                      <div className="PopularCoursesCardLikeWrap">
-                        <img
-                          className="PopularCoursesCardLikeIcon"
-                          src={LikeIcon}
-                          alt="좋아요 아이콘"></img>
-                        <p className="PopularCoursesCardLike">Like</p>
-                      </div>
+                  className="PopularCoursesCardImage"
+                  src={CardDummyImage}
+                  alt="카드 이미지"></img>
+                <div className="PopularCoursesCardInfo">
+                  <div className="PopularCoursesCardTitleWrap">
+                    <span className="PopularCoursesCardTitle">Product 1</span>
+                  </div>
+                  <div className="PopularCoursesCardContentWrap">
+                    <p className="PopularCoursesCardContent">Product 1 data</p>
+                    <div className="PopularCoursesCardLikeWrap">
+                      <img
+                        className="PopularCoursesCardLikeIcon"
+                        src={LikeIcon}
+                        alt="좋아요 아이콘"></img>
+                      <p className="PopularCoursesCardLike">Like</p>
                     </div>
                   </div>
                 </div>
-                <div className="PopularCoursesCard">
-                  <img
-                    className="PopularCoursesCardImage"
-                    src={CardDummyImage}
-                    alt="카드 이미지"></img>
-                  <div className="PopularCoursesCardInfo">
-                    <div className="PopularCoursesCardTitleWrap">
-                      <span className="PopularCoursesCardTitle">Product 1</span>
-                    </div>
-                    <div className="PopularCoursesCardContentWrap">
-                      <p className="PopularCoursesCardContent">
-                        Product 1 data
-                      </p>
-                      <div className="PopularCoursesCardLikeWrap">
-                        <img
-                          className="PopularCoursesCardLikeIcon"
-                          src={LikeIcon}
-                          alt="좋아요 아이콘"></img>
-                        <p className="PopularCoursesCardLike">Like</p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div className="PopularCoursesCard">
-                  <img
-                    className="PopularCoursesCardImage"
-                    src={CardDummyImage}
-                    alt="카드 이미지"></img>
-                  <div className="PopularCoursesCardInfo">
-                    <div className="PopularCoursesCardTitleWrap">
-                      <span className="PopularCoursesCardTitle">Product 1</span>
-                    </div>
-                    <div className="PopularCoursesCardContentWrap">
-                      <p className="PopularCoursesCardContent">
-                        Product 1 data
-                      </p>
-                      <div className="PopularCoursesCardLikeWrap">
-                        <img
-                          className="PopularCoursesCardLikeIcon"
-                          src={LikeIcon}
-                          alt="좋아요 아이콘"></img>
-                        <p className="PopularCoursesCardLike">Like</p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                {/* Card_Dummy */}
-                <img
-                  className="PopularCoursesRightIcon"
-                  src={RightIcon}
-                  alt="오른쪽 아이콘"></img>
               </div>
+              <div className="PopularCoursesCard">
+                <img
+                  className="PopularCoursesCardImage"
+                  src={CardDummyImage}
+                  alt="카드 이미지"></img>
+                <div className="PopularCoursesCardInfo">
+                  <div className="PopularCoursesCardTitleWrap">
+                    <span className="PopularCoursesCardTitle">Product 1</span>
+                  </div>
+                  <div className="PopularCoursesCardContentWrap">
+                    <p className="PopularCoursesCardContent">Product 1 data</p>
+                    <div className="PopularCoursesCardLikeWrap">
+                      <img
+                        className="PopularCoursesCardLikeIcon"
+                        src={LikeIcon}
+                        alt="좋아요 아이콘"></img>
+                      <p className="PopularCoursesCardLike">Like</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              {/* Card_Dummy */}
+              <img
+                className="PopularCoursesRightIcon"
+                src={RightIcon}
+                alt="오른쪽 아이콘"></img>
             </div>
           </div>
         </div>
