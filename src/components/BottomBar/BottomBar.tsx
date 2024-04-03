ed0a02e3140be4f5b052b3a2371059acacd98c69
developersihyeon <139722653+developersihyeon@users.noreply.github.com>
2024-04-03 20:21:24 +0900

    feat :: BottomBar 생성 #5

---
+++ src/components/BottomBar/BottomBar.tsx
@@ -0,0 +1,11 @@
+import "src/components/BottomBar/style/BottomBar.style.css"
+
+const BottomBar = () => {
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+};
+
+export default BottomBar;
