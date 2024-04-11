9338618adb19a771035ec7b10a5b9381e96d031f
이윤선 <108972919+yoonsun123@users.noreply.github.com>
2024-04-11 17:50:36 +0900

    rename : 브랜치 대이동중

--- src/components/ContentPage/Content.tsx
+++ src/components/ContentPage/Content.tsx
@@ -2,13 +2,13 @@ import * as S from "../../style/Content.style";
 import React, { useEffect, useState } from "react";
 import { dummyData } from "./data";
 import Topbar from "../Topbar/Topbar";
-import ReactMarkdown from "react-markdown";
-import heartImg from "../ContentPage/img/heart.svg";
-import nextImg from "../ContentPage/img/next.svg";
-import beforeImg from "../ContentPage/img/before.svg";
-import shareImg from "../ContentPage/img/share.svg";
-import sendImg from "../ContentPage/img/send.svg";
-import topImg from "../ContentPage/img/topImg.svg";
+// import ReactMarkdown from "react-markdown";
+import heartImg from "../../assets/img/heart.svg";
+import nextImg from "../../assets/img/next.svg";
+import beforeImg from "../../assets/img/before.svg";
+import shareImg from "../../assets/img/share.svg";
+import sendImg from "../../assets/img/send.svg";
+import topImg from "../../assets/img/topImg.svg";
 
 const Content: React.FC = () => {
   const markdown = `Just a link: www.nasa.gov.`;
@@ -36,22 +36,41 @@ const Content: React.FC = () => {
             <S.ImgStyle src={dummyData[0].contentImg1} />
             <S.Contents>{dummyData[0].content}</S.Contents>
             <S.Contents>{dummyData[0].content2}</S.Contents>
+            <S.Contents>{dummyData[0].content3}</S.Contents>
             <S.Line />
-            <S.CoverContent>
-              <img src={heartImg} alt="error" />
-              <img src={shareImg} alt="error" />
-            </S.CoverContent>
-            <S.CoverContent>
-              <S.GoToPost>
-                <img src={beforeImg} alt="error" />
-              </S.GoToPost>
-              <S.GoToPost>
-                <img src={nextImg} alt="error" />
-              </S.GoToPost>
-            </S.CoverContent>
-            <textarea>
-              <img src={sendImg} alt="error" />
-            </textarea>
+            <S.BottomBar>
+              <S.BottomItems>
+                <S.BottomImg src={heartImg} alt="error" />
+                Like
+              </S.BottomItems>
+              <S.BottomItems>
+                <S.BottomImg src={shareImg} alt="error" /> Share
+              </S.BottomItems>
+            </S.BottomBar>
+            <S.MovePost>
+              <S.GoToPost_l>
+                <S.GoToPostImg src={beforeImg} alt="error" />
+                <div>
+                  <S.OtherTitle>{dummyData[0].title}</S.OtherTitle>
+                  <S.OtherUser_l>{dummyData[0].user}</S.OtherUser_l>
+                </div>
+              </S.GoToPost_l>
+              <S.GoToPost_r>
+                <div>
+                  <S.OtherTitle>{dummyData[1].title}</S.OtherTitle>
+                  <S.OtherUser_r>{dummyData[1].user}</S.OtherUser_r>
+                </div>
+                <S.GoToPostImg src={nextImg} alt="error" />
+              </S.GoToPost_r>
+            </S.MovePost>
+            <S.Coment>
+              <input
+              type="text"
+              placeholder="Coment"></input>
+              <button>
+                <img src={sendImg} alt="error" />
+              </button>
+            </S.Coment>
           </div>
         </S.ContentStyle>
       </S.All>
