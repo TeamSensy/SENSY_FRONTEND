4dbf9a5785b9a5eca1678c2a49024e415f2a0255
이윤선 <108972919+yoonsun123@users.noreply.github.com>
2024-04-11 17:52:08 +0900

    rename : 개발 코드 대이동(style)

--- src/style/Content.style.ts
+++ src/style/Content.style.ts
@@ -12,13 +12,13 @@ export const ContentStyle = styled.div`
 `;
 
 export const ImgStyle = styled.img`
-  width: 50vw;
+  width: 35vw;
   margin-top:2vh;
   margin-bottom:2vh;
 `;
 export const TopImg = styled.img`
   width: 60vw;
-  margin-bottom: 1vh;
+  margin-bottom: 2vh;
 `;
 
 export const TitleNInfo = styled.div`
@@ -57,8 +57,8 @@ export const Date = styled.span`
 
 export const Line = styled.div`
   border-bottom: 2px solid #F2F2F2;
-  margin-bottom: 1vh;
-  margin-top: 2vh;
+  margin-bottom: 3vh;
+  margin-top: 4vh;
 `;
 
 export const Contents = styled.div`
@@ -68,14 +68,78 @@ font-family: pretendard;
 line-height:25px
 `;
 
-export const GoToPost = styled.div`
+export const GoToPost_l = styled.div`
+display:flex;
+align-items:center;
 background-color: #F8F9FA;
-width : 17vw;
+width : 18vw;
 height : 6vh;
+margin-top:2vh;
 `;
 
-export const CoverContent = styled.div`
+export const GoToPost_r = styled.div`
+display:flex;
+justify-content:flex-end;
+align-items:center;
+background-color: #F8F9FA;
+width : 18vw;
+height : 6vh;
+margin-top:2vh;
+`;
+
+export const OtherTitle = styled.div`
+font-size:13px;
+font-weight:bold;
+font-family: pretendard;
+color:#495057;
+`;
+
+export const OtherUser_r = styled.div`
+font-size:10px;
+font-family: pretendard;
+color:#717F8E;
+display:flex;
+justify-content:flex-end;
+`;
+
+export const OtherUser_l = styled.div`
+font-size:10px;
+font-family: pretendard;
+color:#717F8E;
+`;
+
+export const GoToPostImg = styled.img`
+width:1.7vw;
+margin-left: 1vw;
+margin-right: 1vw;
+`;
+
+export const BottomImg = styled.img`
+width: 1.5vw;
+`;
+
+export const BottomBar = styled.div`
 display: flex;
-background-color:red;
 justify-content : space-between;
+font-size: 13px;
+`;
+
+export const BottomItems = styled.span`
+width:4vw;
+display: flex;
+justify-content : space-between;
+align-items:center;
+`;
+
+export const MovePost = styled.div`
+display: flex;
+justify-content : space-between;
+`;
+
+export const Coment = styled.div`
+display: flex;
+`;
+
+export const Input = styled.input`
+outline: none;
 `;
